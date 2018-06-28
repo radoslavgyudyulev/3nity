695f5dfe56c1c408166b49aa11080403aca5ca5e
Radoslav Gyudyulev <radoslav.gyudyulev@gmail.com>
2018-06-28 16:09:27 +0300

    g

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3001,
-    // DB: 'mongodb://radoslav:rado1994@ds161610.mlab.com:61610/3nity',
-     DB: 'mongodb://localhost:27017/3nity_FEATURE',
+    DB: 'mongodb://radoslav:rado1994@ds161610.mlab.com:61610/3nity',
+    //  DB: 'mongodb://localhost:27017/3nity_FEATURE',
 }
